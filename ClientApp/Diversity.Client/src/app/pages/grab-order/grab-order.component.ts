d2bf0c4f16b0eb2e4f22bb5cf1b4697046ad9c77
asadkhan50255 <asadkhan50255@gmail.com>
2023-01-08 05:50:20 +0800

    fixed-image-issue

--- ClientApp/Diversity.Client/src/app/pages/grab-order/grab-order.component.ts
+++ ClientApp/Diversity.Client/src/app/pages/grab-order/grab-order.component.ts
@@ -29,7 +29,7 @@ export class GrabOrderComponent implements OnInit {
   
 
   getCurrentUserOrder() {
-    debugger
+    this.imageObject=[];
     this.spinner.show();
     this.orderService.getCurrectOrder().subscribe({
       next: (response: any) => {
